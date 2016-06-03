3106bc74909a17c3f978b898fab593e8950c06f7
Nathan Trost <Nathaniel.Trost@vinsolutions.com>
2016-06-03 16:12:49 -0500

    svg icons

---
+++ src/app/rte-icons/icon-remove-link.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class IconRemoveLink extends React.Component {
+  render() {
+    return (
+		<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+			<path d="M19 13H5v-2h14v2z"/>
+		</svg>
+	);
+  }
+}
+
+export default IconRemoveLink;
---
+++ src/app/rte-icons/icons.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import IconAccept from './icon-accept';
+import IconCancel from './icon-cancel';
+
+import IconBlockquote from './icon-blockquote';
+import IconCode from './icon-code';
+
+import IconBold from './icon-bold';
+import IconItalic from './icon-italic';
+import IconStrikethrough from './icon-strikethrough';
+import IconUnderline from './icon-underline';
+
+import IconLeftIndent from './icon-left-indent';
+import IconRightIndent from './icon-right-indent';
+
+import IconLink from './icon-link';
+import IconRemoveLink from './icon-remove-link';
+ 
+import IconRedo from './icon-redo';
+import IconUndo from './icon-undo';
+
+import IconOrderedList from './icon-ordered-list';
+import IconUnorderedList from './icon-unordered-list';
+
+export { 
+	IconAccept, 
+	IconBlockquote, 
+	IconBold, 
+	IconCancel, 
+	IconCode, 
+	IconItalic, 
+	IconLeftIndent,  
+	IconLink, 
+	IconOrderedList, 
+	IconRedo, 
+	IconRemoveLink, 
+	IconRightIndent, 
+	IconStrikethrough, 
+	IconUnderline, 
+	IconUndo, 
+	IconUnorderedList 
+	};
